1776183655e4e9a670e4fffa0cd2663b1f03e459
Bastos Pisoni Paola <paola.bastospisoni@student-cs.fr>
2023-11-15 15:31:30 +0100

    some tests created

--- test_grid.py
+++ test_grid.py
@@ -1,4 +1,5 @@
 from grid_minesweeper import *
 
 def test_create_grid():
-    assert game_grid_create() == []
+    assert test_create_grid() == [[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' '],[' ',' ',' ', ' ']]
+
