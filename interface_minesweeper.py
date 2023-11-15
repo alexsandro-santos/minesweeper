19b93fe595269a5d7760e3280e122bd6d3d36ee2
Rosa De Lima Eduardo <eduardo.rosadelima@student-cs.fr>
2023-11-15 18:20:38 +0100

    Fim do trabalho dia 1

--- interface_minesweeper.py
+++ interface_minesweeper.py
@@ -1,14 +1,16 @@
 from tkinter import *
 from tkinter import ttk
 import random
+from PIL import Image, ImageTk
 
-
-
+colors = ['white','#bae1ff',"#baffc9","#ffffba","#ffdfba","#ffb3ba",
+          "#ffb3ba","#ffb3ba","#ffb3ba","#ffb3ba"]
 def open(i,j, grid):
-    global root
+    global root, photo, colors
     print('Opening case ',i,j)
     pressed = buttons[i][j]
-    new = Button(root, bg="white", fg = 'orange', width=2, height=1,
+    color = colors[grid[i][j]]
+    new = Button(root, bg=color, fg = 'orange', width=2, height=1,
                  font = ('Arial 9 bold'), relief="sunken",
                  text=str(grid[i][j]))
     new.grid(row=i, column=j)
@@ -22,9 +24,11 @@ def open(i,j, grid):
 #     button.config(state="disabled")
 
 def right_click(event):
+    global photo
     bot = event.widget
     if bot["bg"] == "white" and bot["text"] == "":
         event.widget.configure(bg="red")
+        #event.widget.configure(image = photo)
     elif bot["bg"] == "red" and bot["text"] == "":
         event.widget.configure(text="?")
         event.widget.configure(bg="white")
@@ -40,16 +44,21 @@ def scroll_click(event): #UNUSED
         event.widget.configure(bg="white")
 
 def create_grid(n):
-    global buttons, root
+    global buttons, root, photo
     root = Tk()
+    bomb = Image.open("C:/Users/eduar/Downloads/bomb.png")
+    root.wm_iconphoto(False, ImageTk.PhotoImage(bomb))
     root.title("Minesweeper")
     size = int(27.5*n)
-    button_size = int(size/10)
+    button_size = int(size/n)
     root.geometry(str(size)+"x"+str(size))
     root.resizable(0, 0)
+    #flag = Image.open("C:/Users/eduar/Downloads/tile_flag.gif")
+    #photo = ImageTk.PhotoImage(flag)
+    #photo = PhotoImage(file="C:/Users/eduar/Downloads/tile_flag.gif")
     grid = []
     for _ in range(n):
-        grid.append([random.choice([0,1,2,3,'X']) for _ in range (n)])
+        grid.append([random.randint(0,5) for _ in range (n)])
 
     print(grid)
     print(len(grid), len(grid[0]))
@@ -74,12 +83,6 @@ def create_grid(n):
             # Add a reference to the button to 'buttons'
             buttons[i].append(button)
 
-create_grid(10)
+create_grid(9)
 root.mainloop()
-''''button = Button(root, bg="White", width=5, height=1)
-button.grid(row=11, column=11)
-button2 = Button(root, bg="Gray", width=5, height=1)
-button2.grid(row=10, column=10)
-button3 = Button(root, bg="Orange", width=5, height=1)
-button3.grid(row=10, column=10)'''
 
