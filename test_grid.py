090d3b79ef16b5b03c9bf78a0e6e543e14158a6c
Bastos Pisoni Paola <paola.bastospisoni@student-cs.fr>
2023-11-15 21:17:49 +0100

    update - newtests

--- test_grid.py
+++ test_grid.py
@@ -1,23 +1,59 @@
-from grid_minesweeper import *
-
 from game_minesweeper.grid_minesweeper import *
 from game_minesweeper.textual_minesweeper import *
 
-def test_create_grid():
-    assert test_create_grid() == [[' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
-    assert test_create_grid() == []
+game_grid = [[0, 1, 1, 1, 1, -1, 1, 1, 2, 2], [1, 3, -1, 2, 2, 1, 1, 2, -1, -1], [-1, 4, 2, -1, 1, 0, 1, -1, 3, 2], [-1, 3, 1, 2, 2, 1, 2, 2, 2, 0], [-1, 3, 1, 1, -1, 1, 1, -1, 2, 1], [2, -1, 1, 1, 1, 1, 1, 1, 2, -1], [2, 2, 2, 1, 1, 0, 1, 1, 2, 1], [-1, 2, 1, -1, 1, 0, 1, -1, 1, 0], [-1, 3, 1, 2, 2, 2, 2, 3, 2, 1], [-1, 2, 0, 1, -1, 2, -1, 2, -1, 1]]
+
+# def test_create_grid():
+#     assert test_create_grid() == [[' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '], [' ',' ',' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']]
+#     assert test_create_grid() == []
+
+def test_game_grid_size():
+
+    complexity = read_player_difficulty()
+
+    dict_complexity_size = {"easy": 10, "medium": 15, "hard": 20}
+
+    game_grid, state_grid = game_grid_create()
+
+    grid_size = len(game_grid)
+
+    grid_size_expected = dict_complexity_size[complexity]
+
+    assert grid_size == grid_size_expected
+
+def test_state_grid_size():
+    game_grid, state_grid = game_grid_create()
+
+    grid_size = len(state_grid)
+
+    assert grid_size == len(game_grid)
 
 def test_get_bombs_positions():
-    assert test_get_bombs_positions([[0, 1, 1, 1, 1, -1, 1, 1, 2, 2], [1, 3, -1, 2, 2, 1, 1, 2, -1, -1], [0, 1, 2, -1, 1, 0, 1, -1, 2, 1], [0, 0, 1, 1, 1, 0, 2, 2, 2, 0], [1, 1, 1, 0, 0, 0, 1, -1, 1, 0], [] ])
 
+    assert test_get_bombs_positions([[0, 1, 1, 1, 1, -1, 1, 1, 2, 2], [1, 3, -1, 2, 2, 1, 1, 2, -1, -1], [-1, 4, 2, -1, 1, 0, 1, -1, 3, 2], [-1, 3, 1, 2, 2, 1, 2, 2, 2, 0], [-1, 3, 1, 1, -1, 1, 1, -1, 2, 1], [2, -1, 1, 1, 1, 1, 1, 1, 2, -1], [2, 2, 2, 1, 1, 0, 1, 1, 2, 1], [-1, 2, 1, -1, 1, 0, 1, -1, 1, 0], [-1, 3, 1, 2, 2, 2, 2, 3, 2, 1], [-1, 2, 0, 1, -1, 2, -1, 2, -1, 1]]) == [(5, 0), (2, 1), (8, 1), (0, 2), (3, 2), (7, 2), (0, 3), (0, 4), (4, 4), (7, 4), (1, 5), (9, 5), (0, 7), (3, 7), (7, 7), (0, 8), (0, 9), (4, 9), (6, 9), (8, 9)]
 
+def test_get_random_position(game_grid):
 
+    size_grid = len(game_grid)
 
+    random_position = get_random_position(game_grid)
 
-def test_get_bombs_positions():
-    assert test_get_bombs_positions([[0, 1, -1, 0], [64, 0, 32, 2], [2, 2, 8, 4], [512, 8, 16, 0]])==[(0,0),(0,3),(1,1),(3,3)]
-    assert test_get_bombs_positions([[' ', 16, 32, 0], [64, 0, 32, 2], [2, 2, 8, 4], [512, 8, 16, 0]])==[(0,0),(0,3),(1,1),(3,3)]
-    assert test_get_bombs_positions(create_grid(2))==[(0,0),(0,1),(1,0),(1,1)]
-    assert test_get_bombs_positions([[16,4,8,2], [2,4,2,128], [4,512,32,64], [1024,2048,512,2]])==[]
+    assert isinstance(random_position, tuple) and 0 <= random_position[0] < size_grid and 0 <= random_position[1] < size_grid
+    assert game_grid[random_position[0]][random_position[1]] == 0
 
+def test_place_bombs(game_grid):
 
+    assert any(-1 in row for row in game_grid)
+    
+
+def test_get_tile_neighbours(game_grid):
+
+    neighbours_coordinates = get_tile_neighbours(game_grid, 2, 1)
+    
+    assert neighbours_coordinates == [(1, 0), (2, 0), (3, 0), (1, 1), (3, 1), (1, 2), (2, 2), (3, 2)]
+
+def test_tile_number_calculate():
+
+    assert tile_number_calculate([[0, 0, 0, 0, 0, -1, 0, 0, 0, 0], [0, 0, -1, 0, 0, 0, 0, 0, -1, -1], [-1, 0, 0, -1, 0, 0, 0, -1, 0, 0], [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, -1, 0, 0, -1, 0, 0], [0, -1, 0, 0, 0, 0, 0, 0, 0, -1], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [-1, 0, 0, -1, 0, 0, 0, -1, 0, 0], [-1, 0, 0, 0, 0, 0, 0, 0, 0, 0], [-1, 0, 0, 0, -1, 0, -1, 0, -1, 0]]) == [[0, 1, 1, 1, 1, -1, 1, 1, 2, 2], [1, 3, -1, 2, 2, 1, 1, 2, -1, -1], [-1, 4, 2, -1, 1, 0, 1, -1, 3, 2], [-1, 3, 1, 2, 2, 1, 2, 2, 2, 0], [-1, 3, 1, 1, -1, 1, 1, -1, 2, 1], [2, -1, 1, 1, 1, 1, 1, 1, 2, -1], [2, 2, 2, 1, 1, 0, 1, 1, 2, 1], [-1, 2, 1, -1, 1, 0, 1, -1, 1, 0], [-1, 3, 1, 2, 2, 2, 2, 3, 2, 1], [-1, 2, 0, 1, -1, 2, -1, 2, -1, 1]]
+
+    
