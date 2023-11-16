33b61d578ab4f084c37e9f86cf5952e79b22ae9c
Rosa De Lima Eduardo <eduardo.rosadelima@student-cs.fr>
2023-11-16 11:25:23 +0100

    Manhã de quinta feira

--- interface_minesweeper.py
+++ interface_minesweeper.py
@@ -46,50 +46,60 @@ def get_text(grid, i, j):
         return ''
     else:
         return str(nb)
-    
+
+# def startup():
+# Seria interessante mover a criação da root pra cá
+# Na root selecionaria a dificuldade e chamaria a função
+# responsável por criar o mainframe (e o jogo)
+
 def create_grid(grid):
-    global buttons, root, photo
+    global buttons, mainFrame, bomb
     n = len(grid)
     root = Tk()
-    bomb = Image.open("C:/Users/eduar/Downloads/bomb.png")
-    root.wm_iconphoto(False, ImageTk.PhotoImage(bomb))
+    mainFrame = Toplevel()
+    icon = Image.open("C:/Users/eduar/Downloads/bomb.png")
+    root.wm_iconphoto(True, ImageTk.PhotoImage(icon))
     root.title("Minesweeper")
-    size = int(27.5*n)
+    mainFrame.title("Minesweeper")
+    size = int(24*n)
     button_size = int(size/n)
-    root.geometry(str(size)+"x"+str(size))
-    root.resizable(0, 0)
-
+    mainFrame.geometry(str(size)+"x"+str(size))
+    mainFrame.resizable(0, 0)
+    text_size = 20
+    l1= Label(mainFrame, text=bomb+'Minesweeper'+bomb, font= ('Helvetica'+str(text_size)+'underline'))
+    l1.grid(column=1,row=0, columnspan=n+2,sticky=N+E+S+W)
     buttons = [[] for _ in range(len(grid))]
 
     for i in range(len(grid)):
-        root.rowconfigure(i,weight=button_size)
+        mainFrame.rowconfigure(i+2,weight=button_size)
         for j in range(len(grid[i])):
-            root.columnconfigure(j,weight=button_size)
-            button = Button(root, bg="light gray", width=2, height=1,
+            #mainFrame.rowconfigure(i+2,weight=button_size+5)
+            #mainFrame.columnconfigure(j+2,weight=button_size)
+            button = Button(mainFrame, bg="light gray", width=2, height=1,
                             font = ('Helvetica 9 bold'),
                             command=lambda  i=i,j=j, grid=grid: open(i,j,grid))
 
             #button.bind("<Button-1>", left_click)
             button.bind("<Button-2>", right_click)
             button.bind("<Button-3>", right_click)
-            button.grid(row=i, column=j)
+            button.grid(row=i+2, column=j+2)
             buttons[i].append(button)
 
 def open(i,j, grid):
-    global root, colors
+    global mainFrame, colors
     print('Opening case ',i,j)
     pressed = buttons[i][j]
     color = colors[grid[i][j]]
     text = get_text(grid,i,j)
-    new = Button(root, bg=color, fg = 'orange', width=2, height=1,
+    new = Button(mainFrame, bg=color, fg = 'orange', width=2, height=1,
                  font = ('Arial 9 bold'), relief="sunken",
                  text=text)
-    new.grid(row=i, column=j)
+    new.grid(row=i+2, column=j+2)
     buttons[i][j] = new
     new.config(state = 'disabled')
     pressed.destroy()
 
-x=5
+x=20
 grid = []
 for _ in range(x):
     grid.append([random.choice([-1,0,0,0,0,0]) for _ in range (x)])
@@ -126,5 +136,5 @@ print(grid)
 print(len(grid), len(grid[0]))
 
 create_grid(grid)
-root.mainloop()
+mainFrame.mainloop()
 
