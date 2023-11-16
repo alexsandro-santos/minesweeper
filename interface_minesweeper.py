cf54ab63d058decf396ec24b98583888d33c4906
Rosa De Lima Eduardo <eduardo.rosadelima@student-cs.fr>
2023-11-16 10:27:34 +0100

    Representação das bombas e tentativa de contagem

--- interface_minesweeper.py
+++ interface_minesweeper.py
@@ -2,22 +2,13 @@ from tkinter import *
 from tkinter import ttk
 import random
 from PIL import Image, ImageTk
+import html
+
+flag = html.unescape('&#128681')
+qm = html.unescape('&#63')
 
 colors = ['white','#bae1ff',"#baffc9","#ffffba","#ffdfba","#ffb3ba",
-          "#ffb3ba","#ffb3ba","#ffb3ba","#ffb3ba"]
-def open(i,j, grid):
-    global root, photo, colors
-    print('Opening case ',i,j)
-    pressed = buttons[i][j]
-    color = colors[grid[i][j]]
-    new = Button(root, bg=color, fg = 'orange', width=2, height=1,
-                 font = ('Arial 9 bold'), relief="sunken",
-                 text=str(grid[i][j]))
-    new.grid(row=i, column=j)
-    buttons[i][j] = new
-    new.config(state = 'disabled')
-    pressed.destroy()
-    
+          "#ffb3ba","#ffb3ba","#ffb3ba","white"]   
 
 # def left_click(event): #UNUSED
 #     button.config(bg="green")
@@ -26,14 +17,15 @@ def open(i,j, grid):
 def right_click(event):
     global photo
     bot = event.widget
-    if bot["bg"] == "white" and bot["text"] == "":
+    if bot["bg"] == "light gray" and bot["text"] == "":
         event.widget.configure(bg="red")
-        #event.widget.configure(image = photo)
-    elif bot["bg"] == "red" and bot["text"] == "":
-        event.widget.configure(text="?")
-        event.widget.configure(bg="white")
+        event.widget.configure(text = flag)
+    elif bot["text"] == flag:
+        event.widget.configure(text=qm)
+        event.widget.configure(bg="light blue")
     else:
         event.widget.configure(text="")
+        event.widget.configure(bg="light gray")
 
 def scroll_click(event): #UNUSED
     bot = event.widget
@@ -43,8 +35,21 @@ def scroll_click(event): #UNUSED
         event.widget.configure(text="")
         event.widget.configure(bg="white")
 
-def create_grid(n):
+bomb = html.unescape('&#128163')
+
+def get_text(grid, i, j):
+    global bomb
+    nb = grid[i][j]
+    if nb == -1:
+        return bomb
+    elif nb == 0:
+        return ''
+    else:
+        return str(nb)
+    
+def create_grid(grid):
     global buttons, root, photo
+    n = len(grid)
     root = Tk()
     bomb = Image.open("C:/Users/eduar/Downloads/bomb.png")
     root.wm_iconphoto(False, ImageTk.PhotoImage(bomb))
@@ -53,15 +58,6 @@ def create_grid(n):
     button_size = int(size/n)
     root.geometry(str(size)+"x"+str(size))
     root.resizable(0, 0)
-    #flag = Image.open("C:/Users/eduar/Downloads/tile_flag.gif")
-    #photo = ImageTk.PhotoImage(flag)
-    #photo = PhotoImage(file="C:/Users/eduar/Downloads/tile_flag.gif")
-    grid = []
-    for _ in range(n):
-        grid.append([random.randint(0,5) for _ in range (n)])
-
-    print(grid)
-    print(len(grid), len(grid[0]))
 
     buttons = [[] for _ in range(len(grid))]
 
@@ -69,20 +65,66 @@ def create_grid(n):
         root.rowconfigure(i,weight=button_size)
         for j in range(len(grid[i])):
             root.columnconfigure(j,weight=button_size)
-            #n=grid[i][j]
-            button = Button(root, bg="white", width=2, height=1,
+            button = Button(root, bg="light gray", width=2, height=1,
+                            font = ('Helvetica 9 bold'),
                             command=lambda  i=i,j=j, grid=grid: open(i,j,grid))
 
             #button.bind("<Button-1>", left_click)
             button.bind("<Button-2>", right_click)
             button.bind("<Button-3>", right_click)
-
-            # Add the button to the window
             button.grid(row=i, column=j)
-
-            # Add a reference to the button to 'buttons'
             buttons[i].append(button)
 
-create_grid(9)
+def open(i,j, grid):
+    global root, colors
+    print('Opening case ',i,j)
+    pressed = buttons[i][j]
+    color = colors[grid[i][j]]
+    text = get_text(grid,i,j)
+    new = Button(root, bg=color, fg = 'orange', width=2, height=1,
+                 font = ('Arial 9 bold'), relief="sunken",
+                 text=text)
+    new.grid(row=i, column=j)
+    buttons[i][j] = new
+    new.config(state = 'disabled')
+    pressed.destroy()
+
+x=5
+grid = []
+for _ in range(x):
+    grid.append([random.choice([-1,0,0,0,0,0]) for _ in range (x)])
+    
+for i in range(len(grid)):
+    for j in range(len(grid)):
+        if grid[i][j] == -1:
+            try:
+                grid[i][j-1] += 1
+            except: pass
+            try:
+                grid[i][j+1] += 1
+            except: pass
+            try:
+                grid[i-1][j-1] += 1
+            except: pass
+            try:
+                grid[i-1][j] += 1
+            except: pass
+            try:
+                grid[i-1][j+1] += 1
+            except: pass
+            try:
+                grid[i+1][j-1] += 1
+            except: pass
+            try:
+                grid[i+1][j] += 1
+            except: pass
+            try:
+                grid[i+1][j+1] += 1
+            except: pass
+
+print(grid)
+print(len(grid), len(grid[0]))
+
+create_grid(grid)
 root.mainloop()
 
