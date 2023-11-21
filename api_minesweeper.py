fda0d3e5e4ad254a5a470af78ba5e9e92aba5f1a
Santos Da Rosa Junior Alexsandro <alexsandro.santos-da-rosa-jr@student-cs.fr>
2023-11-22 00:16:42 +0100

    Formatting code

--- api_minesweeper.py
+++ api_minesweeper.py
@@ -2,25 +2,34 @@ from interface_minesweeper import interface_minesweeper as gui_ms
 from game_minesweeper import grid_minesweeper as grid_ms
 from game_minesweeper.textual_minesweeper import difficulty
 
+
 def open_all(state_grid, game_grid, x, y):
     for i, row in enumerate(state_grid):
         for j, tile in enumerate(row):
             if tile not in ('f','?',' ') or (i==x and j==y):
                 gui_ms.open_button(i,j,'o',state_grid, game_grid)
 
+
 def start_game():
+
     global diff
+
     root = gui_ms.startup()
     root.wait_variable(gui_ms.diff)
+
     if gui_ms.diff.get() == 'close':
         root.destroy()
+
         return 0,0,0,0
     else:
         n, n_bombs = difficulty[gui_ms.diff.get()]
         _, state_grid = grid_ms.game_grid_create(n)
+
         return n, n_bombs, root, state_grid
 
+
 def play_game(n, n_bombs, root, state_grid):
+
     global mainFrame
     
     try: mainFrame.destroy()
@@ -32,19 +41,18 @@ def play_game(n, n_bombs, root, state_grid):
                            lambda n=n, n_bombs=n_bombs,
                            root = root, state_grid = state_grid:
                            play_game(n,n_bombs,root,state_grid))
+    
     root.wait_variable(gui_ms.x)
     if gui_ms.x.get()>=0:
-        #print(gui_ms.x.get(),gui_ms.y.get())
         click_x = gui_ms.x.get()
         click_y = gui_ms.y.get()
         command = gui_ms.cmd.get()
         game_grid, state_grid = grid_ms.game_grid_init(n, n_bombs, (click_x, click_y))
         state_grid = grid_ms.make_move(game_grid, state_grid, command, click_x, click_y)
-        # gui_ms.open_button(click_x,click_y,command,state_grid, game_grid)
         if command == 'o':
             open_all(state_grid, game_grid, click_x, click_y)
         gui_ms.window_open = True
-    #print(grid_ms.grid_to_string(state_grid))
+
     while not grid_ms.is_game_over(state_grid, n_bombs) and gui_ms.window_open :
         root.wait_variable(gui_ms.x)
         if gui_ms.x.get() >= 0:
@@ -52,28 +60,24 @@ def play_game(n, n_bombs, root, state_grid):
             coordinate_y = gui_ms.y.get() ## receive from gui
             command = gui_ms.cmd.get() ## receive from gui
             state_grid = grid_ms.make_move(game_grid, state_grid, command, coordinate_x, coordinate_y)
-            #print(grid_ms.grid_to_string(state_grid))
             if command == 'o':
                 open_all(state_grid, game_grid, coordinate_x, coordinate_y)
         else:
             gui_ms.window_open = False
     
-    
     if gui_ms.window_open:
-        #print('\nGame closed before the end') # change a state to show endgame status
         gui_ms.rb['bg']='yellow'
         b = gui_ms.Label(mainFrame, text='')
         b.grid(row=1,column=0, columnspan=5)
         if -1 not in grid_ms.get_all_tiles(state_grid):
-            print('\nYou won!') # change a state to show endgame status
             b['text'] = "You won!"
             gui_ms.rb['text'] = gui_ms.winface
             gui_ms.label_count['text'] = 0
         else:
-            print('\nYou lost!') # change a state to show endgame status
             b['text'] = "You lose!"
             gui_ms.rb['text'] = gui_ms.sad
             gui_ms.label_count['text'] = n_bombs
+
 
 if __name__ == '__main__':
     n, n_bombs, root, state_grid = start_game()
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from api_minesweeper import *
 
-if __name__ == '__main__':
+
+def main():
     n, n_bombs, root, state_grid = start_game()
     if n>0:
         play_game(n, n_bombs, root, state_grid)
         root.mainloop()
+
+if __name__ == '__main__':
+    main()
